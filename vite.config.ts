0024a032c494501f6d10d6776d0981e472a4cfb7
MrJohn616 <a351giova1nni385.francisco10@gmail.com>
2024-06-29 17:47:24 -0600

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "https://MrJohn616.github.io/apple-page",
-});
