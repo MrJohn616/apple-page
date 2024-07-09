2ee5d9c717d424571e84ef159ac291c4dfc2d0aa
MrJohn616 <a351giova1nni385.francisco10@gmail.com>
2024-07-08 18:58:22 -0600

    Udpate

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
-import Pruebas from "./Pruebas.tsx";
+// import Pruebas from "./Pruebas.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
